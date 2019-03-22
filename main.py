1c0ff3be15d463f1d02106617d35e591274ccf86
RyoLyolik <41757023+RyoLyolik@users.noreply.github.com>
2019-03-22 19:21:52 +0300

    Llalladw
    asdfdfgn

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import pygame
+import os
+import sys, requests
+from func import Mapping
 
 screen = None
-size = w, h, = 600, 600
+size = w, h, = 600, 450
 pygame.init()
 screen = pygame.display.set_mode(size)
+mapping = Mapping()
 class Win:
     def __init__(self):
+        self.group = pygame.sprite.Group()
         self.screen_update()
     def screen_update(self):
         self.run = True
-        screen.fill((0,0,0))
+        self.map_image = self.load_map(mapping.start)
+        self.map_now = pygame.sprite.Sprite()
+        self.map_now.image = self.map_image
+        self.map_now.rect = self.map_now.image.get_rect()
+        self.group.add(self.map_now)
         while self.run:
+            screen.fill((0, 0, 0))
             for e in pygame.event.get():
                 if e.type == pygame.QUIT:
                     self.run = False
 
-            # DRAWING
+            self.group.draw(screen)
 
             pygame.display.flip()
+
+    def load_map(self, function):
+        name = function()
+        fullname = os.path.join(name)
+        try:
+            image = pygame.image.load(fullname)
+        except pygame.error as message:
+            print('Cannot load image:', name)
+            raise SystemExit(message)
+        print(image)
+        image = image.convert_alpha()
+        return image
+
 
 if __name__ == '__main__':
     win = Win()
